29a33b71b52acf2d70cee5cdf571b3e0cf3b2f9e
unknown <jalilrzq@gmail.com>
2022-06-23 13:16:56 +0700

    create new product

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
+$(() => {
+  $('.new-card').slice(0,4).show()
+  $('.load-more').click(function(e){
+    e.preventDefault()
+    $('.new-card:hidden').slice(0,4).slideDown()
+  })
+})
+
 var swiper = new Swiper(".mySwiper", {
-  // effect: "coverflow",
   grabCursor: true,
   spaceBetween: 20,
   centeredSlides: false,
@@ -8,14 +15,4 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".bxs-chevron-right-circle",
     prevEl: ".bxs-chevron-left-circle",
   },
-  // coverflowEffect: {
-  //   rotate: 50,
-  //   stretch: 0,
-  //   depth: 100,
-  //   modifier: 1,
-  //   slideShadows: true,
-  // },
-  // pagination: {
-  //   el: ".swiper-pagination",
-  // },
 });
